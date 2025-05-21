9474eda190a567aba3c61e31d787acb7e5e76e21
Andrew Shmorhan <140952286+andriishmorhan@users.noreply.github.com>
2025-05-21 13:33:27 +0200

    task 1 - not done
    problem with render, double check

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const API_KEY = '41767782-13727767f44f3e84ebf7a04b0';
+const BASE_URL = 'https://pixabay.com/api/';
+
+export async function getImagesByQuery(query) {
+  const params = {
+    key: API_KEY,
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    per_page: 20,
+  };
+
+  try {
+    const response = await axios.get(BASE_URL, { params });
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching images:', error);
+    throw error;
+  }
+}
---
+++ src/main.js
@@ -0,0 +1,57 @@
+import { getImagesByQuery } from './js/pixabay-api.js';
+
+import {
+  createGallery,
+  clearGallery,
+  showLoader,
+  hideLoader,
+} from './js/render-functions.js';
+
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+const input = form.elements['search-text'];
+
+form.addEventListener('submit', onSubmit);
+
+async function onSubmit(event) {
+  event.preventDefault();
+  const searchQuery = input.value.trim();
+
+  if (searchQuery === '') {
+    iziToast.warning({
+      title: 'Warning',
+      message: 'Please enter a search term.',
+      position: 'topRight',
+    });
+    return;
+  }
+
+  clearGallery();
+  showLoader();
+
+  try {
+    const data = await getImagesByQuery(searchQuery);
+    if (data.hits.length === 0) {
+      iziToast.info({
+        title: 'Info',
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
+        position: 'topRight',
+      });
+      return;
+    }
+
+    createGallery(data.hits);
+  } catch (error) {
+    iziToast.error({
+      title: 'Error',
+      message: 'Failed to fetch images. Please try again later.',
+      position: 'topRight',
+    });
+    console.error(error);
+  } finally {
+    hideLoader();
+  }
+}
